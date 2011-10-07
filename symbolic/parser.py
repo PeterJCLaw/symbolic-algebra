2274faf990f88c2115e2b6d24cf6c092ebf325b5
Peter Law <PeterJCLaw@gmail.com>
2011-10-07 23:00:07 +0100

    Add a basic start to the parsing -- a tokenising method.

--- symbolic/parser.py
+++ symbolic/parser.py
@@ -1,4 +1,37 @@
 
+import re
+from string import whitespace
+
+class ParseException(Exception):
+	pass
+
 class Parser(object):
+	_operators = '=*/+-'
+	_punctuation = _operators[0] + '()' + _operators[1:]
+
 	def parse(self, string):
+		string = string.strip()
+		print self._punctuation
+
+		for token in self.tokenise(string):
+			print "'"+token+"'"
+
 		return None
+
+	def tokenise(self, string):
+		token = ''
+		for s in string:
+			# token boundary
+			if s in whitespace and token != '':
+				yield token
+				token = ''
+			elif s in self._punctuation and token != '':
+				yield token
+				token = ''
+				yield s
+			elif s.isalnum():
+				token += s
+			else:
+				raise ParseException("Got invalid token '%s'." % s)
+
+		yield token
