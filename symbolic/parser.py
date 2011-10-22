cc73d00a1bbccb858dabc4860d21564294ba5916
Peter Law <PeterJCLaw@gmail.com>
2011-10-22 15:48:23 +0100

    Implement grouping.
    Strings to parse can now use brackets "(" and ")" to override the
    default precendence of operators.

--- symbolic/parser.py
+++ symbolic/parser.py
@@ -19,10 +19,13 @@ class ParseException(Exception):
 
 class Parser(object):
 	_operators = '^*/+-='
-	_punctuation = '()' + _operators
+	_leftBracket = '('
+	_rightBracket = ')'
+	_punctuation = _leftBracket + _rightBracket + _operators
 
 	def parse(self, string):
 		string = string.strip()
+		self._groupStack = []
 		self._leftStack = []
 	#	print 'Parsing:', string
 
@@ -35,6 +38,15 @@ class Parser(object):
 				self._appendOrMerge(Value(int(token)))
 			elif token[0].isalpha() and token.isalnum():
 				self._appendOrMerge(Symbol(token))
+			elif token is self._leftBracket:
+				self._groupStack.append(self._leftStack)
+				self._leftStack = []
+			elif token is self._rightBracket:
+				oldLeft = self._leftStack
+			#	print 'oldLeft', oldLeft
+				self._leftStack = self._groupStack.pop()
+			#	print 'popped ', self._leftStack
+				self._appendOrMerge(oldLeft)
 			else:
 				raise ParseException("Got unexpected token '%s'." % token)
 		#	print self._leftStack
@@ -42,7 +54,9 @@ class Parser(object):
 		if len(self._leftStack) == 0:
 			return None
 
-		return self._leftStack[0]
+		tree = self._removeGrouping(self._leftStack)
+	#	print tree
+		return tree
 
 	def _addOperator(self, opType):
 		# get one from the stack, or error
@@ -80,6 +94,14 @@ class Parser(object):
 		firstPos  = self._operators.index(first._type)
 		secondPos = self._operators.index(second._type)
 		return firstPos < secondPos
+
+	def _removeGrouping(self, tree):
+		if isinstance(tree, list):
+			tree = tree[0]
+		if isinstance(tree, Operator):
+			tree._left  = self._removeGrouping(tree._left)
+			tree._right = self._removeGrouping(tree._right)
+		return tree
 
 	def tokenise(self, string):
 		token = ''
